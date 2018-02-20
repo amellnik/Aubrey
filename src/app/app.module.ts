f69ac3b8f45442e4e91c16991c8e6d3330479d63
Alex Mellnik <a.r.mellnik@gmail.com>
2018-02-19 16:19:32 -0800

    An Ionic app to scan QR codes
    and upload their contents and location information to a databse

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { HttpClientModule } from '@angular/common/http';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { Geolocation } from '@ionic-native/geolocation';
+import { QRScanner} from '@ionic-native/qr-scanner';
+
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { ApiProvider } from '../providers/api/api';
 
 @NgModule({
   declarations: [
@@ -14,6 +20,7 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -24,7 +31,10 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    Geolocation,
+    QRScanner,
+    ApiProvider
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+// import { HttpErrorResponse } from '@angular/common/http';
+import { NavController, AlertController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
+import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner';
+
+import { ApiProvider } from './../../providers/api/api';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +13,92 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    private geolocation: Geolocation,
+    private qrScanner: QRScanner,
+    public alertCtrl: AlertController,
+    public apiProvider: ApiProvider
+  ) {
+    console.log("Home constructor")
 
   }
+
+  lat: number;
+  long: number;
+  qrText: string;
+
+  go() {
+    this.geolocation.getCurrentPosition().then((resp) => {
+     console.log(resp.coords.latitude + ',' + resp.coords.longitude);
+     this.lat = resp.coords.latitude;
+     this.long = resp.coords.longitude;
+     this.doScan();
+    }).catch((error) => {
+      console.log('Error getting location', error.message);
+      this.showAlert('Error getting location', error.message);
+    });
+  }
+
+
+  doScan() {
+    this.qrScanner.prepare().then((status: QRScannerStatus) => {
+     if (status.authorized) {
+       let scanSub = this.qrScanner.scan().subscribe((text: string) => {
+         console.log('Scanned something', text);
+         this.qrText = text;
+         this.qrScanner.hide(); // hide camera preview
+         window.document.querySelector('ion-app').classList.remove('transparent-body');
+         scanSub.unsubscribe(); // stop scanning
+
+         this.apiProvider.sendUpdate(this.lat, this.long, this.qrText).subscribe(res => {
+           this.showAlert('Update sent', text);
+         }, err => {
+           this.showAlert('POST error', err.error);
+         })
+       });
+
+       this.qrScanner.show();
+       window.document.querySelector('ion-app').classList.add('transparent-body');
+
+     } else if (status.denied) {
+       console.log("I need the camera to scan...");
+       this.showAlert('I need the camera to scan', "Status denied");
+     } else {
+       console.log("I need the camera to scan...");
+       this.showAlert('I need the camera to scan', "Other error");
+     }
+  })
+  .catch((e: any) => {
+    console.log('Error is', e);
+    this.showAlert('QR code error', e);
+    });
+  }
+
+  showAlert(title:string, message:string) {
+    let alert = this.alertCtrl.create({
+      title: title,
+      subTitle: message,
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
+testAPI() {
+  this.apiProvider.test().subscribe(res => {
+    this.showAlert('It worked', JSON.stringify(res));
+  }, err => {
+    this.showAlert('It did not work', JSON.stringify(err));
+  })
+}
+
+sendFakeUpdate() {
+  this.apiProvider.sendFakeUpdate().subscribe(res => {
+    this.showAlert('It worked', JSON.stringify(res));
+  }, err => {
+    this.showAlert('It did not work', JSON.stringify(err));
+  })
+}
+
 
 }
